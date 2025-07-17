7478849e467a7024b341b3bfa2d0488e4dea249a
yuzmaryporras <yuzmaporras@gmail.com>
2025-07-17 09:06:00 -0400

    fix: rename the table

--- src/products/entities/product-image.entity.ts
+++ src/products/entities/product-image.entity.ts
@@ -1,7 +1,7 @@
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { Product } from "./product.entity";
 
-@Entity()
+@Entity({ name: 'product_images'})
 export class ProductImage {
 
     @PrimaryGeneratedColumn()
@@ -12,7 +12,8 @@ export class ProductImage {
 
     @ManyToOne(
         () => Product,
-        ( product ) => product.images
+        ( product ) => product.images,
+        { onDelete: 'CASCADE' }
     )
     product: Product
 }
