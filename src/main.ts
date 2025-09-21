dbed1d528dbd6c24cf4cfcd8855e30c95259c2b6
yuzmaryporras <yuzmaporras@gmail.com>
2025-09-21 18:07:12 -0300

    feat: add logger to bootstrap function for improved startup logging

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Bootstrap');   
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
@@ -12,5 +13,6 @@ async function bootstrap() {
     })
   )
   await app.listen(process.env.PORT ?? 3000);
+  logger.log(`App running on port ${ process.env.PORT }`);
 }
 bootstrap();
